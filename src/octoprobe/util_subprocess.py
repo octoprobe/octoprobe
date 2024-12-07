eeedf7823fc828b981c7addee73f66af92f4778a
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2024-12-07 17:10:44 +1100

    subprocess_run() now redirects directly to a file

--- src/octoprobe/util_subprocess.py
+++ src/octoprobe/util_subprocess.py
@@ -6,44 +6,76 @@ import time
 logger = logging.getLogger(__file__)
 
 
-def subprocess_run(args: list[str], cwd: pathlib.Path, timeout_s: float = 10.0) -> str:
+def subprocess_run(
+    args: list[str],
+    cwd: pathlib.Path,
+    logfile: pathlib.Path | None = None,
+    timeout_s: float = 10.0,
+) -> None:
     """
     Wrappsr around 'subprocess()'
     """
     assert isinstance(args, list)
     assert isinstance(cwd, pathlib.Path)
+    assert isinstance(logfile, pathlib.Path | None)
     assert isinstance(timeout_s, float | None)
 
     args_text = " ".join(args)
 
     begin_s = time.monotonic()
     try:
-        proc = subprocess.run(
-            args,
-            check=False,
-            capture_output=True,
-            text=True,
-            cwd=str(cwd),
-            timeout=timeout_s,
-        )
+        if logfile is not None:
+            logger.info(f"EXEC {args_text}")
+            logger.info(f"EXEC     stdout: {logfile}")
+            with logfile.open("w") as f:
+                f.write(f"{' '.join(args)}\n\n\n")
+                f.flush()
+                proc = subprocess.run(
+                    # Common args
+                    args=args,
+                    check=False,
+                    text=True,
+                    cwd=str(cwd),
+                    timeout=timeout_s,
+                    # Specific args
+                    stdout=f,
+                    stderr=subprocess.STDOUT,
+                )
+                f.write(f"\n\nreturncode={proc.returncode}\n")
+                f.write(f"duration={time.monotonic()-begin_s:0.3f}s\n")
+        else:
+            proc = subprocess.run(
+                # Common args
+                args=args,
+                check=False,
+                text=True,
+                cwd=str(cwd),
+                timeout=timeout_s,
+                # Specific args
+                capture_output=True,
+            )
     except subprocess.TimeoutExpired as e:
         logger.info(f"EXEC {e!r}")
         logger.exception(e)
         raise
 
-    stdout = proc.stdout.strip()
-    stderr = proc.stderr.strip()
-
     def log(f) -> None:
         f(f"EXEC {args_text}")
-        f(f"  duration={time.monotonic()-begin_s:0.3f}s")
         f(f"  returncode={proc.returncode}")
-        f(f"  stdout: {stdout}")
-        f(f"  stderr: {stderr}")
+        f(f"  duration={time.monotonic()-begin_s:0.3f}s")
+        if logfile is not None:
+            f(f"  logfile={logfile}")
+        else:
+            stdout = proc.stdout.strip()
+            stderr = proc.stderr.strip()
+            f(f"  stdout: {stdout}")
+            f(f"  stderr: {stderr}")
 
     log(logger.debug)
+
     if proc.returncode != 0:
         log(logger.warning)
-        raise ValueError(f"EXEC failed with returncode={proc.returncode}: {args_text}")
-
-    return stdout
+        msg = f"EXEC failed with returncode={proc.returncode}: {args_text}"
+        if logfile is not None:
+            msg += f"\nlogfile={logfile}"
+        raise ValueError(msg)
