e2fe34d65461b5146328f9388de5d3ec9d9cdb42
Hans Maerki <buhtig.hans.maerki@ergoinfo.ch>
2026-04-30 05:43:25 +0200

    flakyness: Switch off all timeouts

--- src/octoprobe/util_constants_uart_flakiness.py
+++ src/octoprobe/util_constants_uart_flakiness.py
@@ -8,14 +8,14 @@ Several optimizations are done on guessing.
 These constants allow to switch this optimizations on/off and if not required anymore: Remove them.
 """
 
-SUBPROCESS_TENTACLE_DUT_TIMEOUT = True
+SUBPROCESS_TENTACLE_DUT_TIMEOUT = False
 """
 Switch the power of the DUT of before the subprocess gets killed.
 """
 
-DUT_POWER_OFF_TIME_MIN_S: float | None = 2.0
+DUT_POWER_OFF_TIME_MIN_S: float | None = None  # 2.0
 """
 If a DUT is powered off, it should remain unpowered for some time.
 """
 
-TIMEOUT_AFTER_DUT_OFF_S: float | None = 2.0
+TIMEOUT_AFTER_DUT_OFF_S: float | None = None  # 2.0
